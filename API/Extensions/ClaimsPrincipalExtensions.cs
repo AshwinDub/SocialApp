ea4192ae16b4a74c48a7ac3d29245583fc88f6f5
Ashwin Dubey <ashwin.dubey95@gmail.com>
2021-06-12 01:31:32 +0400

    initial commit

--- API/Extensions/ClaimsPrincipalExtensions.cs
+++ API/Extensions/ClaimsPrincipalExtensions.cs
@@ -5,13 +5,7 @@ namespace API.Extensions
 {
     public static class ClaimsPrincipalExtensions
     {
-        public static string GetUserName(this ClaimsPrincipal user)
-        {
-            return user.FindFirst(ClaimTypes.Name)?.Value;
-        }
-        public static int GetUserId(this ClaimsPrincipal user)
-        {
-            return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value);
-        }
+        public static string GetUserName(this ClaimsPrincipal user) => user.FindFirst(ClaimTypes.Name)?.Value;
+        public static int GetUserId(this ClaimsPrincipal user) => int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value);
     }
 }
